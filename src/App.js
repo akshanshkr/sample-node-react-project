a1ce8c6d153955e67025826622cf0c1ef1810b6d
akshanshkr <akshansh.kumar@chicmic.co.in>
2024-05-21 11:29:17 +0530

    remove jashan

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to totally new page final commit check jashan </h1>
+          <h1 className="App-title">Welcome to totally new page final commit check  </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload change ddsdfs.
